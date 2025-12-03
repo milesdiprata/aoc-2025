5b4adb05eaa16e44b2a1830e5969c58c99271a3a
Miles di Prata <milesdiprata3@gmail.com>
2025-12-03 08:45:21 -0800

    Day 3 part 1

---
+++ src/day3.rs
@@ -0,0 +1,91 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::str::FromStr;
+
+use anyhow::anyhow;
+use anyhow::Error;
+use anyhow::Result;
+
+#[derive(Debug)]
+struct BatteryBank {
+    joltages: Vec<u8>,
+}
+
+impl FromStr for BatteryBank {
+    type Err = Error;
+
+    fn from_str(joltages: &str) -> Result<Self> {
+        if joltages.is_empty() {
+            return Err(anyhow!("empty joltage ratings"));
+        }
+
+        let joltages = joltages
+            .chars()
+            .map(|joltage| joltage.to_digit(10))
+            .collect::<Option<Vec<_>>>()
+            .ok_or_else(|| anyhow!("invalid joltage rating"))?;
+
+        if joltages.len() < Self::K {
+            return Err(anyhow!(format!("bank size less than {}", Self::K)));
+        }
+
+        #[allow(clippy::cast_possible_truncation)]
+        Ok(Self {
+            joltages: joltages.into_iter().map(|joltage| joltage as u8).collect(),
+        })
+    }
+}
+
+impl BatteryBank {
+    const K: usize = 2;
+
+    fn joltage_max(&self) -> u8 {
+        fn dfs(joltages: &[u8], idx: usize, subset: &mut Vec<u8>, max: &mut u8) {
+            if subset.len() == 2 {
+                *max = (*max).max((10 * subset[0]) + subset[1]);
+                return;
+            }
+
+            if idx == joltages.len() {
+                return;
+            }
+
+            subset.push(joltages[idx]);
+            dfs(joltages, idx + 1, subset, max);
+
+            subset.pop();
+            dfs(joltages, idx + 1, subset, max);
+        }
+
+        let mut subset = Vec::with_capacity(Self::K);
+        let mut max = 0;
+        dfs(&self.joltages, 0, &mut subset, &mut max);
+        max
+    }
+}
+
+fn part1(banks: &[BatteryBank]) -> u64 {
+    banks
+        .iter()
+        .map(BatteryBank::joltage_max)
+        .map(u64::from)
+        .sum()
+}
+
+fn main() -> Result<()> {
+    let banks = BufReader::new(File::open("in/day3.txt")?)
+        .lines()
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .map(|line| line.parse::<BatteryBank>())
+        .collect::<Result<Vec<_>>>()?;
+
+    let part1 = self::part1(&banks);
+
+    println!("Part 1: {part1}");
+
+    assert_eq!(part1, 17_346);
+
+    Ok(())
+}
