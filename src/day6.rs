3c097c67296027c7ed809f82580897cc85d6b322
Miles di Prata <milesdiprata3@gmail.com>
2025-12-07 09:29:13 -0800

    Day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -5,6 +5,10 @@ use anyhow::anyhow;
 use anyhow::Error;
 use anyhow::Result;
 
+trait Solvable {
+    fn problems(&self) -> &[Problem];
+}
+
 #[derive(Debug)]
 enum Problem {
     Add(Vec<usize>),
@@ -12,6 +16,10 @@ enum Problem {
 }
 
 struct Homework {
+    problems: Vec<Problem>,
+}
+
+struct Homework2 {
     problems: Vec<Problem>,
 }
 
@@ -48,11 +56,13 @@ impl FromStr for Homework {
             .map(Iterator::collect::<Result<Vec<_>, _>>)
             .collect::<Result<Vec<_>, _>>()?;
 
-        for col in 0..nums[0].len() {
-            for row in 0..nums.len() {
-                match &mut problems[col] {
-                    Problem::Add(problem) | Problem::Mul(problem) => problem.push(nums[row][col]),
-                }
+        let row_len = nums.len();
+        let col_len = nums.first().map(Vec::len).unwrap_or_default();
+
+        #[allow(clippy::needless_range_loop)]
+        for col in 0..col_len {
+            for row in 0..row_len {
+                problems[col].push(nums[row][col]);
             }
         }
 
@@ -60,14 +70,83 @@ impl FromStr for Homework {
     }
 }
 
-fn part1(homework: &Homework) -> usize {
+impl FromStr for Homework2 {
+    type Err = Error;
+
+    fn from_str(input: &str) -> Result<Self> {
+        let mut lines = input.lines().collect::<Vec<_>>();
+
+        let mut problems = lines
+            .pop()
+            .ok_or_else(|| anyhow!("empty input"))?
+            .split_ascii_whitespace()
+            .map(Problem::from_str)
+            .collect::<Result<Vec<_>>>()?;
+
+        let grid = lines
+            .into_iter()
+            .map(|line| line.chars().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        let row_len = grid.len();
+        let col_len = grid.first().map(Vec::len).unwrap_or_default();
+
+        let mut idx = problems.len().saturating_sub(1);
+
+        for col in (0..col_len).rev() {
+            if (0..row_len).all(|row| grid[row][col].is_ascii_whitespace()) {
+                idx = idx.saturating_sub(1);
+                continue;
+            }
+
+            let mut num = 0usize;
+
+            #[allow(clippy::needless_range_loop)]
+            for row in 0..row_len {
+                if let Some(digit) = grid[row][col].to_digit(10) {
+                    num *= 10;
+                    num += digit as usize;
+                }
+            }
+
+            problems[idx].push(num);
+        }
+
+        Ok(Self { problems })
+    }
+}
+
+impl Solvable for Homework {
+    fn problems(&self) -> &[Problem] {
+        self.problems.as_slice()
+    }
+}
+
+impl Solvable for Homework2 {
+    fn problems(&self) -> &[Problem] {
+        self.problems.as_slice()
+    }
+}
+
+impl Problem {
+    fn push(&mut self, num: usize) {
+        match self {
+            Self::Add(nums) | Self::Mul(nums) => nums.push(num),
+        }
+    }
+}
+
+fn solve<S>(homework: &S) -> usize
+where
+    S: Solvable,
+{
     let mut sum = 0;
 
-    for problem in &homework.problems {
+    for problem in homework.problems() {
         sum += match problem {
             Problem::Add(nums) => nums.iter().sum::<usize>(),
             Problem::Mul(nums) => nums.iter().product::<usize>(),
-        }
+        };
     }
 
     sum
@@ -75,13 +154,18 @@ fn part1(homework: &Homework) -> usize {
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("in/day6.txt")?;
-    let homework = Homework::from_str(input.as_str())?;
 
-    let part1 = self::part1(&homework);
+    let homework = Homework::from_str(input.as_str())?;
+    let homework2 = Homework2::from_str(input.as_str())?;
+
+    let part1 = self::solve(&homework);
+    let part2 = self::solve(&homework2);
 
     println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
 
     assert_eq!(part1, 4_405_895_212_738);
+    assert_eq!(part2, 7_450_962_489_289);
 
     Ok(())
 }
