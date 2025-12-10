bec94fe7648e4cfcc6b44e015442e2a94e2719b4
Miles di Prata <milesdiprata3@gmail.com>
2025-12-10 09:16:43 -0800

    Day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,3 +1,5 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::fs;
 use std::str::FromStr;
@@ -45,6 +47,7 @@ impl JunctionBox {
     }
 }
 
+/// Union find
 fn part1(boxes: &[JunctionBox]) -> usize {
     fn find(parents: &mut [usize], i: usize) -> usize {
         if parents[i] != i {
@@ -86,6 +89,55 @@ fn part1(boxes: &[JunctionBox]) -> usize {
     lens.into_iter().take(3).product()
 }
 
+/// Kruskal's MST
+fn part2(boxes: &[JunctionBox]) -> i64 {
+    fn find(parents: &mut [usize], i: usize) -> usize {
+        if parents[i] != i {
+            parents[i] = find(parents, parents[i]);
+        }
+        parents[i]
+    }
+
+    let mut pairs = Vec::new();
+    for i in 0..boxes.len() - 1 {
+        for j in i + 1..boxes.len() {
+            pairs.push((i, j, boxes[i].dist(&boxes[j])));
+        }
+    }
+
+    let mut pairs = pairs
+        .into_iter()
+        .map(|(i, j, d)| Reverse((d, i, j)))
+        .collect::<BinaryHeap<_>>();
+
+    let mut parents = (0..boxes.len()).collect::<Vec<_>>();
+    let mut lens = vec![1; boxes.len()];
+    let mut component_len_max = 1;
+
+    while let Some(Reverse((_, i, j))) = pairs.pop() {
+        let root_i = find(&mut parents, i);
+        let root_j = find(&mut parents, j);
+
+        if root_i != root_j {
+            if lens[root_i] < lens[root_j] {
+                parents[root_i] = root_j;
+                lens[root_j] += lens[root_i];
+                component_len_max = component_len_max.max(lens[root_j]);
+            } else {
+                parents[root_j] = root_i;
+                lens[root_i] += lens[root_j];
+                component_len_max = component_len_max.max(lens[root_i]);
+            }
+        }
+
+        if component_len_max == boxes.len() {
+            return boxes[i].coords[0] * boxes[j].coords[0];
+        }
+    }
+
+    unreachable!()
+}
+
 fn main() -> Result<()> {
     let input = fs::read_to_string("in/day8.txt")?;
     let boxes = input
@@ -94,10 +146,13 @@ fn main() -> Result<()> {
         .collect::<Result<Vec<_>>>()?;
 
     let part1 = self::part1(&boxes);
+    let part2 = self::part2(&boxes);
 
     println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
 
     assert_eq!(part1, 129_564);
+    assert_eq!(part2, 42_047_840);
 
     Ok(())
 }
